895e90656ab7283f525411b027a072fae07e3f69
Md Najrul Ansari <114806322+najru01@users.noreply.github.com>
2025-07-09 22:17:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,3 @@ async def remove_background(file: UploadFile = File(...)):
         "filename": file.filename,
         "image": output_data.hex()
     }
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 4000))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
